9625bd08a530447413592e25ab4f21ea3efad337
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-02-25 14:53:07 +0800

    Update VendingMachineDemo.java

--- VendingMachineDemo.java
+++ VendingMachineDemo.java
@@ -1,30 +1,30 @@
 public class VendingMachineDemo {
     public static void main(String[] args) {
-        VendingMachine machine = new VendingMachine();
-        machine.insertCoin();
+        VendingMachine machine = new VendingMachine(5);
+        machine.insertCoin(5.0);
         machine.dispenseItem();
-        machine.selectItem();
+        machine.selectItem("Coca-Cola");
         System.out.println();
 
-        machine.selectItem();
+        machine.selectItem("Fanta");
         machine.dispenseItem();
-        machine.insertCoin();
+        machine.insertCoin(10.0);
         System.out.println();
 
-        machine.selectItem();
-        machine.insertCoin();
+        machine.selectItem("Pepsi");
+        machine.insertCoin(4.0);
         machine.setOutOfOrder();
         machine.dispenseItem();
         System.out.println();
 
-        machine.insertCoin();
+        machine.insertCoin(6.0);
         machine.dispenseItem();
-        machine.selectItem();
+        machine.selectItem("RC");
         machine.setOutOfOrder();
         System.out.println();
 
-        machine.selectItem();
-        machine.insertCoin();
+        machine.selectItem("Mountain Dew");
+        machine.insertCoin(14.0);
         machine.dispenseItem();
         machine.setOutOfOrder();
     }
