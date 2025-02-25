7a20e0c6dc02113da86af716fddb3ad8c38d7c6e
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-02-25 14:52:11 +0800

    Update ItemSelectedState.java

--- ItemSelectedState.java
+++ ItemSelectedState.java
@@ -1,18 +1,19 @@
 public class ItemSelectedState implements VendingMachineState {
     @Override
-    public void selectItem(VendingMachine machine) {
+    public void selectItem(VendingMachine machine, String item) {
         System.out.println("Item already selected");
     }
 
     @Override
-    public void insertCoin(VendingMachine machine) {
-        System.out.println("Coin inserted");
+    public void insertCoin(VendingMachine machine, double amount) {
+        System.out.println("Inserted coin: $" + amount);
+        machine.setBalance(machine.getBalance() + amount);
         machine.setState(new DispensingState());
     }
 
     @Override
     public void dispenseItem(VendingMachine machine) {
-        System.out.println("Please insert a coin first");
+        System.out.println("Please insert coins first");
     }
 
     @Override
