c1eca2cef9f8cff54cf52ba56d9231f7d753c4d4
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-02-25 14:53:55 +0800

    Update VendingMachineState.java

--- VendingMachineState.java
+++ VendingMachineState.java
@@ -1,6 +1,6 @@
 public interface VendingMachineState {
-    public void selectItem(VendingMachine machine);
-    public void insertCoin(VendingMachine machine);
+    public void selectItem(VendingMachine machine, String item);
+    public void insertCoin(VendingMachine machine, double amount);
     public void dispenseItem(VendingMachine machine);
     public void setOutOfOrder(VendingMachine machine);
 }
