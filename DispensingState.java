a355e7155628ecc8c49d6fed8ae9dbd27f4115e0
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-02-25 14:51:51 +0800

    Update DispensingState.java

--- DispensingState.java
+++ DispensingState.java
@@ -1,22 +1,24 @@
 public class DispensingState implements VendingMachineState {
     @Override
-    public void selectItem(VendingMachine machine) {
+    public void selectItem(VendingMachine machine, String item) {
         System.out.println("Currently dispensing. Cannot select an item");
     }
 
     @Override
-    public void insertCoin(VendingMachine machine) {
+    public void insertCoin(VendingMachine machine, double amount) {
         System.out.println("Currently dispensing. Cannot insert coins");
     }
 
     @Override
     public void dispenseItem(VendingMachine machine) {
         System.out.println("Dispensing complete");
+        machine.setItemInventory(machine.getItemInventory() - 1);
+        machine.setBalance(machine.getBalance() - 1);
         machine.setState(new IdleState());
     }
-
+    
     @Override
     public void setOutOfOrder(VendingMachine machine) {
-        System.out.println("Item still dispensing");
+        System.out.println("Item still dispensing.");
     }
 }
