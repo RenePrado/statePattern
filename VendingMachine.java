4a3d4b87b6f98b760ce1174df0dd828892628e78
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-02-25 14:52:49 +0800

    Update VendingMachine.java

--- VendingMachine.java
+++ VendingMachine.java
@@ -1,20 +1,40 @@
 public class VendingMachine {
     private VendingMachineState state;
+    private int itemInventory;
+    private double balance;
 
-    public VendingMachine() {
+    public VendingMachine(int initialInventory) {
         this.state = new IdleState();
+        this.itemInventory = initialInventory;
+        this.balance = 0.0;
     }
 
     public void setState(VendingMachineState state) {
         this.state = state;
     }
 
-    public void selectItem() {
-        state.selectItem(this);
+    public int getItemInventory() {
+        return itemInventory;
     }
 
-    public void insertCoin() {
-        state.insertCoin(this);
+    public void setItemInventory(int itemInventory) {
+        this.itemInventory = itemInventory;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void selectItem(String item) {
+        state.selectItem(this, item);
+    }
+
+    public void insertCoin(double amount) {
+        state.insertCoin(this, amount);
     }
 
     public void dispenseItem() {
